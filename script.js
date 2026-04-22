9b218cf3b07faba6c0bc775c8c3c423f6d4f84b3
Sayanti-C <chatterjisayanti@gmail.com>
2026-04-22 17:54:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-// preload all images
-let preloadedImages = [];
-
-for (let i = 0; i < images.length; i++) {
-    let img = new Image();
-    img.src = images[i];
-    preloadedImages.push(img);
-}
 // Messages
 let messages = [
-    "Maaaa....💖",
-    "আমি কিছু বলবো ...",
-    "You are my biggest strength 🌸",
-    "তুমি সব থেকে cutest person আমার life e🥺",
-    "I love you so... so... much🌍",
-    "!!! Happy Birthday !!!🎂"
+"Maaaa....💖",
+"আমি কিছু বলবো ...",
+"You are my biggest strength 🌸",
+"তুমি সব থেকে cutest person আমার life e🥺",
+"I love you so... so... much🌍",
+"!!! Happy Birthday !!!🎂"
 ];
 
 // Images (same order as messages)
 let images = [
-    "me_smiley.png",
-    "me_cute.png",
-    "mum_me.png",
-    "mum_cute.png",
-    "mum_papa.png",
-    "mum.png"
+"me_smiley.png",
+"me_cute.png",
+"mum_me.png",
+"mum_cute.png",
+"mum_papa.png",
+"mum.png"
 ];
 
 let index = 0;
@@ -32,54 +24,52 @@ let index = 0;
 let bear = document.getElementById("bear");
 
 function nextMessage() {
-    index = (index + 1) % messages.length;
+index = (index + 1) % messages.length;
 
-    let msg = document.getElementById("message");
-    let img = document.getElementById("photo");
+let msg = document.getElementById("message");
+let img = document.getElementById("photo");
 
-    msg.classList.remove("fade");
+msg.classList.remove("fade");
 
-    // change text immediately
-    msg.innerText = messages[index];
+setTimeout(() => {
+msg.innerText = messages[index];
+img.src = images[index];
+msg.classList.add("fade");
 
-    // change image directly (no waiting)
-    img.src = images[index];
-
-    msg.classList.add("fade");
-
-    // 🎆 FINAL MESSAGE EFFECTS
-    if (index === messages.length - 1) {
-        launchConfetti();
-        showBear();
-    }
+// 🎆 FINAL MESSAGE
+if (index === messages.length - 1) {
+launchConfetti();
+showBear();
 }
-  
+
+}, 100);
+}
 
 function createBalloon() {
-    let balloon = document.createElement("div");
-    balloon.classList.add("balloon");
+let balloon = document.createElement("div");
+balloon.classList.add("balloon");
 
-    // random position
-    balloon.style.left = Math.random() * 100 + "vw";
+// random position
+balloon.style.left = Math.random() * 100 + "vw";
 
-    // random speed
-    balloon.style.animationDuration = (4 + Math.random() * 4) + "s";
+// random speed
+balloon.style.animationDuration = (4 + Math.random() * 4) + "s";
 
-    // 🎯 ADD CLICK TO POP
-    balloon.addEventListener("click", () => {
-        balloon.classList.add("pop");
+// 🎯 ADD CLICK TO POP
+balloon.addEventListener("click", () => {
+balloon.classList.add("pop");
 
-        // remove after animation
-        setTimeout(() => {
-            balloon.remove();
-        }, 300);
-    });
+// remove after animation
+setTimeout(() => {
+balloon.remove();
+}, 300);
+});
 
-    document.getElementById("balloon-container").appendChild(balloon);
+document.getElementById("balloon-container").appendChild(balloon);
 
-    setTimeout(() => {
-        balloon.remove();
-    }, 8000);
+setTimeout(() => {
+balloon.remove();
+}, 8000);
 }
 
 // create balloons
@@ -89,54 +79,54 @@ setInterval(createBalloon, 600);
 setInterval(createBalloon, 600);
 
 // 🎆 CONFETTI FUNCTION
-    function launchConfetti() {
-    for (let i = 0; i < 200; i++) {
-        let confetti = document.createElement("div");
-        confetti.classList.add("confetti");
+function launchConfetti() {
+for (let i = 0; i < 200; i++) {
+let confetti = document.createElement("div");
+confetti.classList.add("confetti");
 
-        // RANDOM horizontal start
-        confetti.style.left = Math.random() * 100 + "vw";
+// RANDOM horizontal start
+confetti.style.left = Math.random() * 100 + "vw";
 
-        // RANDOM vertical start (IMPORTANT FIX)
-        confetti.style.top = Math.random() * 50 + "vh";
+// RANDOM vertical start (IMPORTANT FIX)
+confetti.style.top = Math.random() * 50 + "vh";
 
-        // RANDOM COLORS
-        let colors = ["#ff4d6d", "#ffd43b", "#69db7c", "#4dabf7", "#b197fc"];
-        confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+// RANDOM COLORS
+let colors = ["#ff4d6d", "#ffd43b", "#69db7c", "#4dabf7", "#b197fc"];
+confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 
-        // RANDOM SIZE
-        let size = 5 + Math.random() * 8;
-        confetti.style.width = size + "px";
-        confetti.style.height = size + "px";
+// RANDOM SIZE
+let size = 5 + Math.random() * 8;
+confetti.style.width = size + "px";
+confetti.style.height = size + "px";
 
-        // RANDOM DIRECTION (LEFT/RIGHT SPREAD)
-        let randomX = (Math.random() - 0.5) * 200; 
+// RANDOM DIRECTION (LEFT/RIGHT SPREAD)
+let randomX = (Math.random() - 0.5) * 200; 
 
-        confetti.style.setProperty('--x', randomX + 'px');
+confetti.style.setProperty('--x', randomX + 'px');
 
-        // RANDOM SPEED
-        confetti.style.animationDuration = (2 + Math.random() * 2) + "s";
+// RANDOM SPEED
+confetti.style.animationDuration = (2 + Math.random() * 2) + "s";
 
-        document.body.appendChild(confetti);
+document.body.appendChild(confetti);
 
-        setTimeout(() => {
-            confetti.remove();
-        }, 4000);
-    }
+setTimeout(() => {
+confetti.remove();
+}, 4000);
 }
-    
+}
+
 function showBear() {
-    let bear = document.getElementById("bear");
-    bear.style.display = "block";
+let bear = document.getElementById("bear");
+bear.style.display = "block";
 
-    // Move randomly every 1.5 sec
-    setInterval(() => {
-        let x = Math.random() * (window.innerWidth - 120);
-        let y = Math.random() * (window.innerHeight - 120);
+// Move randomly every 1.5 sec
+setInterval(() => {
+let x = Math.random() * (window.innerWidth - 120);
+let y = Math.random() * (window.innerHeight - 120);
 
-        bear.style.left = x + "px";
-        bear.style.top = y + "px";
-    }, 1500);
+bear.style.left = x + "px";
+bear.style.top = y + "px";
+}, 1500);
 }
 
 window.onload = function () {
